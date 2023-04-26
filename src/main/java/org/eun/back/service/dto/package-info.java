ba5fba49fdc5ac512b37ae8aa54187f570231662
tanu <anuartoiguliyev.1@gmail.com>
2023-04-26 12:35:21 +0600

    Initial version of eunReportDesigner generated by generator-jhipster@7.9.3

---
+++ src/main/java/org/eun/back/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package org.eun.back.service.dto;
---
+++ src/main/java/org/eun/back/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package org.eun.back.web.rest.vm;
