49a45f82001399464fb0e55a05586180e191965a
tanu <anuartoiguliyev.1@gmail.com>
2023-07-12 11:13:25 +0600

    fixed docker files

--- src/main/docker/start.sh
+++ src/main/docker/start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Запуск Docker Compose файлов
-docker-compose -f jhipster-control-center.yml up -d
-docker-compose -f monitoring.yml up -d
+#docker-compose -f jhipster-control-center.yml up -d
+#docker-compose -f monitoring.yml up -d
 docker-compose -f postgresql.yml up -d
-docker-compose -f sonar.yml up -d
-docker-compose -f zipkin.yml up -d
+#docker-compose -f sonar.yml up -d
+#docker-compose -f zipkin.yml up -d
 
 
 echo "All Docker Compose files have been started."
