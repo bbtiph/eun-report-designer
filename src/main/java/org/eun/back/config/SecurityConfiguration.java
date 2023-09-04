466a83ee460843b44dd11b2474e3ba3dffc32217
tanu <anuartoiguliyev.1@gmail.com>
2023-09-04 17:58:48 +0600

    - made api dashboards permit all

--- src/main/java/org/eun/back/config/SecurityConfiguration.java
+++ src/main/java/org/eun/back/config/SecurityConfiguration.java
@@ -107,6 +107,7 @@ public class SecurityConfiguration {
             .antMatchers("/api/register").permitAll()
             .antMatchers("/api/activate").permitAll()
             .antMatchers("/api/account/reset-password/init").permitAll()
+            .antMatchers("/api/dashboard/**").permitAll()
             .antMatchers("/api/account/reset-password/finish").permitAll()
             .antMatchers("/api/admin/**").hasAuthority(AuthoritiesConstants.ADMIN)
             .antMatchers("/api/**").authenticated()
